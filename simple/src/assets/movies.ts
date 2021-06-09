ce16e8b4e5d6594e02c4c970399efc9732c7f3ce
soufianebef <83958370+soufianebef@users.noreply.github.com>
2021-06-09 14:08:52 -0400

    add two movies and posters

---
+++ simple/src/assets/movies.ts
@@ -0,0 +1,18 @@
+export const Movies = [
+    {
+        title : "Into the Wild",
+        year : 2007,
+        director : "Sean Penn",
+        genre: "adventure drama",
+        ratingRT: 83,
+        posterPath:"../assets/IntoWild.jpg"
+    },
+    {
+        title : "The Transporter",
+        year : 2002,
+        director : "Corey Yuen",
+        genre: "action",
+        ratingRT: 54,
+        posterPath:"../assets/The transporter.jpeg"
+    }
+];
